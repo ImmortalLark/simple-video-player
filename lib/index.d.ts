88cfe74dc2c7e3ecbcb093118f224700e5070e50
frorice <yesiamfengfan@gmail.com>
2023-10-24 19:35:43 +0800

    Delete redundant folders

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import Video from "./video";
-declare class Player {
-    private container;
-    video: Video;
-    constructor(elementId: string);
-    private initCore;
-    private initVideo;
-}
-export default Player;
